5e08413ab5c0f24b2c32d4f643bbb70243ee5ed7
xiexuanzheng123 <349328984@qq.com>
2017-05-20 23:46:54 +0800

    using static of express routing

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.get('/',function(req, res) {
     res.send('Hello World!');
 });
 
+app.use(express.static(__dirname));
+
 var server = app.listen(3000,function() {
     console.log('Server is Ok ~')
 });
