4f5b0d1688b885152f030997a3721cad6a13f96a
xiexuanzheng123 <349328984@qq.com>
2017-05-20 23:54:23 +0800

    the process of 404

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/',function(req, res) {
 
 app.use(express.static(__dirname));
 
+app.use(function(req, res, next) {
+    res.status(404).send('Sorry can not find that !');
+});
+
 var server = app.listen(3000,function() {
     console.log('Server is Ok ~')
 });
