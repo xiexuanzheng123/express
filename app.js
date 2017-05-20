53e87dafc5bb47ace329f2a2c48f3443f43228d3
xiexuanzheng123 <349328984@qq.com>
2017-05-21 00:06:52 +0800

    using all of routing

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req, res) {
-    res.send('Hello World!');
+// app.get('/',function(req, res) {
+//     res.send('Hello World!');
+// });
+app.all('/secret', function(req, res, next) {
+    console.log('Accessing the secret section...');
+    next(); //对于一个路径上的所有请求加载中间件
 });
 
 app.use(express.static(__dirname));
